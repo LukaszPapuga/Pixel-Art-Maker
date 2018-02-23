a77955e895bcd5c0f48a09c6f9b245cbda5abcca
LukaszPapuga <36710020+LukaszPapuga@users.noreply.github.com>
2018-02-23 22:13:35 +0100

    Create desings.js

---
+++ js/desings.js
@@ -0,0 +1,26 @@
+function makeGrid() {
+  var dcRows = $("#input_height").val();
+  var dcCols = $("#input_width").val();
+  var dcTable = $("#pixel_canvas");
+  dcTable.children().remove(); 
+  for (var row = 0; row < dcRows; row++) {
+    dcTable.append("<tr></tr>");
+    for (var col = 0; col < dcCols; col++) {
+      dcTable.children().last().append("<td></td>");
+    }
+  }
+  
+  dcTable.on("click", "td", function() {
+    var color = $("input[type='color']").val();
+      $(this).attr("bgcolor", color);
+  });
+}
+
+$("input[type='submit']").click(function(creation) {
+  creation.preventDefault();
+  makeGrid();
+});
+
+$('#refresh').click(function() {
+    location.reload();
+});
